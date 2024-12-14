1b40e9a8b7b6c4c21546e176232acc1056c55757
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-12-14 15:28:13 -0600

    [SAMBL Button] Fixed conflictions with Spotify User URLs
    The script now checks to see if the matches the regex before trying to index it
    
    https://github.com/Lioncat6/MusicBrainz-UserScripts/issues/4

--- Musicbrainz-Artist-SAMBL-Button.user.js
+++ Musicbrainz-Artist-SAMBL-Button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Musicbrainz Artist SAMBL Button
 // @description  Adds a SAMBL Button to compatible artists
-// @version      2024-12-06
+// @version      2024-12-14
 // @author       Lioncat6
 // @license      MIT
 // @namespace    https://github.com/Lioncat6/MusicBrainz-UserScripts/
@@ -14,26 +14,28 @@
 // @updateURL    https://github.com/Lioncat6/MusicBrainz-UserScripts/raw/main/Musicbrainz-Artist-SAMBL-Button.js
 // ==/UserScript==
 
-(function () {
-	"use strict";
-	let artistName = document.getElementsByClassName("artistheader")[0].getElementsByTagName("h1")[0];
-	let mbid = document.location.pathname.split("/artist/")[1].split("/")[0];
-	let spIcons = document.getElementsByClassName("spotify-favicon");
-	if (spIcons) {
-		let spids = "";
-		for (let icon of spIcons) {
-			let spUrl = icon.getElementsByTagName("a")[0].href;
-			let spId = spUrl.match(/\/artist\/([^/?]+)/)[1];
-			if (spids == "") {
-				spids = spId;
-			} else {
-				spids = spids + "," + spId;
-			}
-		}
+(function() {
+    "use strict";
+    let artistName = document.getElementsByClassName("artistheader")[0].getElementsByTagName("h1")[0];
+    let mbid = document.location.pathname.split("/artist/")[1].split("/")[0];
+    let spIcons = document.getElementsByClassName("spotify-favicon");
+    if (spIcons) {
+        let spids = "";
+        for (let icon of spIcons) {
+            let spUrl = icon.getElementsByTagName("a")[0].href;
+            if (spUrl.match(/\/artist\/([^/?]+)/)) {
+                let spId = spUrl.match(/\/artist\/([^/?]+)/)[1];
+                if (spids == "") {
+                    spids = spId;
+                } else {
+                    spids = spids + "," + spId;
+                }
+            }
+        }
         let htmlToInsert = `<a href="https://lioncat6.github.io/SAMBL/artist/?spid=${spids}&artist_mbid=${mbid}" target="_blank"><img src="https://lioncat6.github.io/SAMBL/assets/images/favicon.svg" alt="SAMBL Icon" style="width: 18px; transform: scale(1.8) translate(6px, 2.5px);"></a>`;
-        if (spIcons.length > 1){
+        if (spIcons.length > 1) {
             htmlToInsert = `<a href="https://lioncat6.github.io/SAMBL/artist/?spids=${spids}&artist_mbid=${mbid}" target="_blank"><img src="https://lioncat6.github.io/SAMBL/assets/images/favicon.svg" alt="SAMBL Icon" style="width: 18px; transform: scale(1.8) translate(6px, 2.5px);"></a>`;
         }
-		artistName.innerHTML = artistName.innerHTML + htmlToInsert;
-	}
+        artistName.innerHTML = artistName.innerHTML + htmlToInsert;
+    }
 })();
